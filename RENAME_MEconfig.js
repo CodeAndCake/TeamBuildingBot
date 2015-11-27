d60333de233a5bb96cc010a49259311e4bc33087
Matteo Menapace <hello@baddeo.com>
2015-11-27 14:44:35 +0000

    config file to be RENAMED by you

---
+++ RENAME_MEconfig.js
@@ -0,0 +1,18 @@
+module.exports = 
+{
+	keys:
+	{
+		consumer_key: 'YOUR_TWITTER_APP_CONSUMER_KEY',
+		consumer_secret: 'YOUR_TWITTER_APP_CONSUMER_SECRET',
+		access_token_key: 'YOUR_TWITTER_APP_TOKEN_KEY',
+		access_token_secret: 'YOUR_TWITTER_APP_TOKEN_SECRET'
+	},
+	users: 
+	[
+		'SOME_TWITTER_HANDLE', // eg: 'POTUS' or 'Snowden' (without @ in front)
+		'CHANGE_THIS',
+		'AND_THIS',
+		'YOU_CAN_ADD_AS_MANY_AS_YOU_LIKE'
+	],
+	howManySentences: 3 // how many sentences to generate and tweet?
+}
